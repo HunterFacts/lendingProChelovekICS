318e9a43ce112614753f4aa5eba899fdcfdfcbe4
HunterFacts <hunterfacts@mail.ru>
2019-05-26 19:17:29 +0500

    Поправил макет

--- js/init.js
+++ js/init.js
@@ -1,9 +1,46 @@
 (function($){
   $(function(){
-
+	$('.datepicker').datepicker({
+		yearRange: 100
+	});
+	$('select').formSelect();
     $('.sidenav').sidenav();
     $('.parallax').parallax();
 	$('.tabs').tabs();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+function Toast (){
+	var toastHTML;
+	var toastDisplayLength = 16000;
+	if ($("#validater-for-email").text() == "некорректный email" || $("#email_inline").val() == ""){
+		toastDisplayLength = 5000;
+		toastHTML = '<span>Укажите правильный email</span>';
+		}
+	else {
+		toastHTML = '<span>Поиск результатов не дал. В случае нахождения соответствий по вашему запросу информация будет направлена на указанный почтовый адрес</span><button onclick="M.Toast.dismissAll();" class="btn-flat toast-action">Закрыть</button>';		
+	}
+	
+	M.toast({html: toastHTML, displayLength: toastDisplayLength});	
+}
+function Search () {
+	var toastHTML;
+	var toastDisplayLength;
+	
+	if ($("#validater-for-email").text() == "некорректный email" || $("#email_inline").val() == ""){
+		toastDisplayLength = 5000;
+		toastHTML = '<span>Укажите правильный email</span>';
+		M.toast({html: toastHTML, displayLength: toastDisplayLength});
+		}
+	else {
+		var answer = 'По вашему запросу получено 2 результата';
+		$('#search, #h5-date-heirs').hide();
+		$('#h6-answer').text(answer);
+		$('.btn-search, #h6-answer').show();
+	}	
+}
+function UndoSearch() {
+	$('#search, #h5-date-heirs').show();
+	$('.btn-search, #h6-answer').hide();
+}
